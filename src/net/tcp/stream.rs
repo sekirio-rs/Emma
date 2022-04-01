3ad9c59c3b282b484e269430d5a7d90e17efb5cc
SKTT1Ryze <linuxgnulover@gmail.com>
2022-04-01 11:04:22 +0800

    add []

--- src/net/tcp/stream.rs
+++ src/net/tcp/stream.rs
@@ -1,1 +1,15 @@
+use std::net;
+use std::os::unix::io::{FromRawFd, RawFd};
 
+pub struct TcpStream {
+    // currently [`std::net::TcpStream`]
+    inner: net::TcpStream,
+}
+
+impl TcpStream {
+    pub unsafe fn from_raw_fd(fd: RawFd) -> Self {
+        Self {
+            inner: net::TcpStream::from_raw_fd(fd),
+        }
+    }
+}
