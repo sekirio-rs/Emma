e5c7fc504fb50f435248e14ab1b6a8b40e682c65
SKTT1Ryze <linuxgnulover@gmail.com>
2022-04-17 14:55:15 +0800

    add close op

---
+++ src/io/close.rs
@@ -0,0 +1,21 @@
+use super::op::Op;
+use crate::Emma;
+use crate::Result;
+use io_uring::{opcode, types};
+use std::os::unix::io::RawFd;
+
+pub struct Close {
+    fd: RawFd,
+}
+
+impl<'emma> Op<'emma, Close> {
+    pub(crate) fn async_close(emma: &'emma Emma, fd: RawFd) -> Result<Op<'emma, Close>> {
+        Op::async_op(emma, move |token| {
+            let entry = opcode::Close::new(types::Fd(fd))
+                .build()
+                .user_data(token as _);
+
+            (entry, Close { fd })
+        })
+    }
+}
