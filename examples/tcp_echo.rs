8ac186c9788df08bae043af1251c17a24d624869
SKTT1Ryze <linuxgnulover@gmail.com>
2022-04-26 11:41:38 +0800

    update alias and example

--- examples/tcp_echo.rs
+++ examples/tcp_echo.rs
@@ -1,8 +1,5 @@
+use emma::alias::*;
 use emma::net::tcp::listener::TcpListener;
-use emma::net::tcp::stream::TcpStream;
-use emma::Emma;
-use emma::Join;
-use emma::Reactor;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -15,62 +12,19 @@ fn main() -> io::Result<()> {
 
         let listener = TcpListener::bind("127.0.0.1:3344").unwrap();
 
-        let stream = accept(&emma, &listener).await.unwrap();
+        let stream = accept_socket(&emma, &listener).await.unwrap();
 
         let mut buf = [0u8; 1024];
 
         loop {
             // recv
-            let n = recv(&emma, &mut buf, &stream).await.unwrap();
+            let n = recv_msg(&emma, &mut buf, &stream).await.unwrap();
 
             if n == 0 {
                 return Ok(());
             }
 
-            send(&emma, &buf, &stream).await.unwrap();
+            send_msg(&emma, &buf, &stream).await.unwrap();
         }
     })
-}
-
-async fn accept(emma: &Emma, listener: &TcpListener) -> io::Result<TcpStream> {
-    let accept_fut = listener.accept(emma).map_err(|e| e.as_io_error())?;
-
-    let reactor = Reactor::new(emma);
-    let mut join_fut = Join::new(reactor);
-
-    join_fut.as_mut().join(accept_fut);
-
-    let (stream, _) = join_fut
-        .await
-        .map(|mut ret| ret.remove(0).unwrap())
-        .map_err(|e| e.as_io_error())?;
-
-    Ok(stream)
-}
-
-async fn recv(emma: &Emma, buf: &mut [u8; 1024], stream: &TcpStream) -> io::Result<usize> {
-    let recv_fut = stream.recv(emma, buf).map_err(|e| e.as_io_error())?;
-
-    let mut join_fut = Join::new(Reactor::new(emma));
-
-    join_fut.as_mut().join(recv_fut);
-
-    let res = join_fut
-        .await
-        .map(|mut ret| ret.remove(0).unwrap().uring_res as _)
-        .map_err(|e| e.as_io_error())?;
-
-    Ok(res)
-}
-
-async fn send(emma: &Emma, buf: &[u8; 1024], stream: &TcpStream) -> io::Result<()> {
-    let send_fut = stream.send(emma, buf).map_err(|e| e.as_io_error())?;
-
-    let mut join_fut = Join::new(Reactor::new(emma));
-
-    join_fut.as_mut().join(send_fut);
-
-    let _ = join_fut.await.map_err(|e| e.as_io_error())?;
-
-    Ok(())
 }
